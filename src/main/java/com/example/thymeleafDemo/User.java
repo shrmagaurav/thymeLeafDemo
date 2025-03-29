1346bdb59011d21b57ff9f546d7c6766365cbf5d
Gaurav Sharma <gaurav.sharma@sarvika.com>
2025-03-30 04:38:24 +0530

    initial commit

---
+++ src/main/java/com/example/thymeleafDemo/User.java
@@ -0,0 +1,48 @@
+package com.example.thymeleafDemo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
